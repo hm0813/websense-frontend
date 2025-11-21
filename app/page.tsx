2083f788b22c8005f8a70fd9ca42838959bde7d9
hm0813 <97744148+hm0813@users.noreply.github.com>
2025-11-21 19:31:17 +0530

    Update frontend with latest code

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
     setLoading(true);
 
     try {
-      const res = await fetch("https://websense-backend-eyi5.onrender.com/search", {
+      const res = await fetch("http://127.0.0.1:8000/search", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url, query }),
